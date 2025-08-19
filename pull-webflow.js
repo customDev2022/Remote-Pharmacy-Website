cc4e7deb645ea6fa6b83e9a5069b763777bd225d
souvik <souvik.mondal@codeclouds.com>
2025-08-19 21:23:01 +0530

    initial webflow set up

---
+++ pull-webflow.js
@@ -0,0 +1,18 @@
+import fetch from "node-fetch";
+import fs from "fs";
+
+const WEBFLOW_SITE_ID = "the-site-id-remoteph";
+const WEBFLOW_API_KEY = "the-api-key-remoteph";
+
+const headers = {
+  "accept-version": "1.0.0",
+  Authorization: `Bearer ${WEBFLOW_API_KEY}`,
+};
+
+async function fetchCollections(siteId) {
+  const url = `https://api.webflow.com/sites/${WEBFLOW_SITE_ID}/collections`;
+  const response = await fetch(url, { headers });
+  const data = await response.json();
+  fs.writeFileSync("collections.json", JSON.stringify(data, null, 2));
+  console.log("collections synced from webflow");
+}
